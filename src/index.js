8a89f5d56848f6f50e0f3f129aabd771208ccf34
ps807 <66031843+ps807@users.noreply.github.com>
2020-09-25 15:13:03 +0200

    END

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+  textAlign: "center"
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
